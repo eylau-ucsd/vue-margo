399b13e5621004af100f40c282a74a32312de816
eylau-ucsd <eylau@ucsd.edu>
2022-06-29 19:40:45 +0200

    fix defineProps undefined error, add basic global styling to each listing

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,17 @@ const listings = reactive([
 
 <template>
     <div :key="listing.id" v-for="listing in listings">
-        <ListingCard data="listing" />
+        <ListingCard :data="listing" />
     </div>
 </template>
+
+<style>
+.listing-card {
+  border-color: black;
+  border-style: solid;
+  border-width: 5px;
+  padding: 5px;
+  margin-top: 8px;
+  margin-bottom: 8px;
+}
+</style>
--- src/components/ListingCard.vue
+++ src/components/ListingCard.vue
@@ -1,12 +1,11 @@
 <script setup>
+import { defineProps } from "vue";
 defineProps({
     data: {
         type: Object,
         required: true
     }
 })
-
-
 </script>
 
 <template>
